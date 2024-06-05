f813daf5f32985791d62f436b494d7a74df34b2d
Mauricio Bellon <mauriciobellon@gmail.com>
2024-06-04 22:56:28 -0300

    Update Config.h

--- Config.h
+++ Config.h
@@ -1,18 +1,44 @@
 #include <SPIFFS.h>
+#include <ArduinoJson.h>
 
+class Config {
+public:
+    char ssid[32];
+    char password[32];
+    char hostname[32];
 
-class Config{
-    public:
-        char ssid[32];
-        char password[32];
-        char hostname[32];
+    void LoadConfig() {
+        if (SPIFFS.exists("/config.json")) {
+            File configFile = SPIFFS.open("/config.json", "r");
+            if (configFile) {
+                size_t size = configFile.size();
+                std::unique_ptr<char[]> buf(new char[size]);
 
-        void LoadConfig(){
-            if (SPIFFS.exists("/config.json")){
-                
+                configFile.readBytes(buf.get(), size);
+                DynamicJsonDocument doc(1024);
+                deserializeJson(doc, buf.get());
+
+                strcpy(ssid, doc["ssid"]);
+                strcpy(password, doc["password"]);
+                strcpy(hostname, doc["hostname"]);
+
+                configFile.close();
             }
-            else{
-                
-            }
-        };
+        } else {
+            strcpy(ssid, "default_ssid");
+            strcpy(password, "default_password");
+            strcpy(hostname, "esp32_thermostat");
+        }
+    }
+
+    void SaveConfig() {
+        DynamicJsonDocument doc(1024);
+        doc["ssid"] = ssid;
+        doc["password"] = password;
+        doc["hostname"] = hostname;
+
+        File configFile = SPIFFS.open("/config.json", "w");
+        serializeJson(doc, configFile);
+        configFile.close();
+    }
 };
