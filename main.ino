d04308cb311adadabca556760b46188fd1e0527c
Mauricio Bellon <mauriciobellon@gmail.com>
2024-06-04 22:56:50 -0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -3,14 +3,12 @@
   by Mauricio Faria Bellon
   http://github.com/mauriciobellon/esp32-thermostat
   
-  
   This is a simple refrigerator thermostat for ESP32
   It uses a temperature sensor and a relay to control the compressor
   It has a web interface to configure the temperature and the wifi connection
   
   This code is in the public domain.
 **********/
-
 
 #include <WiFi.h>
 #include <WiFiClient.h>
@@ -20,80 +18,104 @@
 #include <OneWire.h>
 #include <Config.h>
 
+Config config;
 
-void LoadConfig(){
-    if (SPIFFS.exists("/config.json")){
-    }
-    else{
-        // set default variables
-    }
-};
+OneWire oneWire(0); // GPIO where the DS18B20 is connected
+DallasTemperature sensors(&oneWire);
 
+const int relayPin = 5;
+const int ledPin = 4;
 
-void pinSetup(){};
-
-void wifiStart(){};
-
-void wifiConnect(){};
-
-void isWifiConnected(){};
-
-void logging(char *event){
-    // log lattest 1000 events
-};
-
-void factoryReset(){
-    // Erases config file
-    // Reboot
-};
-
-void setupFactory(){
-    // pinSetup()
-    // start Wifi AP
-    // serves setup page
-    // wait POST configs
-    // save config file
-    // reboot
-};
-
-void setupOperation(){
-    // pinSetup()
-    // start Display
-    // start Thermostat
-    // read Config File
-    // start Wifi Client
-    // serves operation page
-    //  wifi status
-    //  temp status
-    //  config
-};
-
-void loopOperation(){
-    // read temp
-    // check wifi if disconnect try to connect
-    // compare temp
-    // start or stop cooling
-    // update display
-    // log info
-};
-
-void setup()
-{
-  Serial.begin(115200);
-  if (hasConfigFile())
-  {
-    setupOperation()
-  }
-  else
-  {
-    setupFactory()
-  }
+bool hasConfigFile() {
+    return SPIFFS.exists("/config.json");
 }
 
-void loop()
-{
-  // if factory reset button is high triggers factoryReset()
-  // if hasConfigFile triggers loopOperation()
+void pinSetup() {
+    pinMode(relayPin, OUTPUT);
+    pinMode(ledPin, OUTPUT);
+}
 
-  delay(1000)
+void wifiStart() {
+    WiFi.begin(config.ssid, config.password);
+}
+
+void wifiConnect() {
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+    }
+}
+
+bool isWifiConnected() {
+    return WiFi.status() == WL_CONNECTED;
+}
+
+void logging(const char *event) {
+    // log latest 1000 events
+}
+
+void factoryReset() {
+    SPIFFS.remove("/config.json");
+    ESP.restart();
+}
+
+void setupFactory() {
+    pinSetup();
+    WiFi.softAP("ESP32_Setup");
+    // Serve setup page and wait for POST configs
+    // Save config file and reboot
+}
+
+void setupOperation() {
+    pinSetup();
+    sensors.begin();
+    config.LoadConfig();
+    wifiStart();
+    // Serve operation page
+    // Display wifi status, temp status, config
+}
+
+void loopOperation() {
+    sensors.requestTemperatures();
+    float temperature = sensors.getTempCByIndex(0);
+
+    if (!isWifiConnected()) {
+        wifiConnect();
+    }
+
+    // Add your temperature threshold logic here
+    if (temperature > 25.0) { // Example threshold
+        digitalWrite(relayPin, HIGH); // Start cooling
+    } else {
+        digitalWrite(relayPin, LOW); // Stop cooling
+    }
+
+    // Update display
+    // Log info
+}
+
+void setup() {
+    Serial.begin(115200);
+    if (!SPIFFS.begin(true)) {
+        Serial.println("SPIFFS Mount Failed");
+        return;
+    }
+    if (hasConfigFile()) {
+        setupOperation();
+    } else {
+        setupFactory();
+    }
+}
+
+void loop() {
+    // if factory reset button is high triggers factoryReset()
+    if (digitalRead(2) == HIGH) { // Example factory reset button GPIO
+        factoryReset();
+    }
+
+    // if hasConfigFile triggers loopOperation()
+    if (hasConfigFile()) {
+        loopOperation();
+    }
+
+    delay(1000);
 }
